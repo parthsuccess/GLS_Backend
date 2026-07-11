4a49de389f510b79085f1e3fdf9ee20634dbac36
parthsuccess <patelparth.success@gmail.com>
2026-07-11 16:26:00 +0530

    ymllll

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.get('/ping', (req, res) => {
     res.send({
-        message: 'Hello, GLS!'
+        message: 'Hello, GLS! Welcome to the workshop of Node.js and express.js'
     });
 });
 
